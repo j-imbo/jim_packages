1ffec4e21f47fc3834d3201b94cc24caf76ab391
jmkohler <jameskohler1839+jimgit@gmail.com>
2020-10-23 19:31:44 -0500

    clean/organize macros

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -8,511 +8,330 @@ function has(item, amount)
   end
 end
 
-function soth()
-  local faron = has("sohf")
-  local eldin = has("sohe")
-  local lanayru = has("sohl")
-  local complete = has("sohc")
+-- item functions to make file cleaner
 
-  return (faron and eldin and lanayru) or complete
+function bmb()
+  return has("bombs")
 end
 
-function bottle()
-  local pouch = has("adventure")
-  local bot = has("bottle")
-
-  return (pouch and bot)
+function bow()
+  return has("bow")
 end
 
-function thunderhead()
-  local thc = has("op_thc")
-  local bog = has("bog")
+function slng()
+  return has("sling")
+end
 
-  if thc then
-    return bog
+function btl()
+  return has("beetle")
+end
+
+function hook()
+  return has("hook")
+end
+
+function gust()
+  return has("bellows")
+end
+
+function whip()
+  return has("whip")
+end
+
+function wds()
+  return has("wds")
+end
+
+function cs()
+  return has("cs")
+end
+
+function dig()
+  return has("mitts")
+end
+
+function harp()
+  return has("harp")
+end
+
+function ranged()
+  return (slng() or cs() or btl() or bow() or bomb())
+end
+
+function soth() -- song of the hero
+  return (has("sohf") and has("sohe") and has("sohl")) or has("soh",3)
+end
+
+function bottle() -- can use bottles
+  return (has("adventure") and has("bottle"))
+end
+
+function thunderhead() -- open/closed thunderhead
+  if has("op_thc") then
+    return has("bog")
   else
     return 1
   end
 end
 
-function beedle()
-  local s = has("sling")
-  local bt = has("beetle")
-  local bw = has("bow")
-  local cs = has("cs")
-  local bm = has("bombs")
-  return (s or bt or bw or cs or bm)
+function walls() -- goddess walls
+  return (skyview2_acc() and has("bog") and harp())
 end
+
+-- area macros
 
 function sealedtemple()
-  local em = has("emerald")
-  
-  return em
-end
-
-function eldin1()
-  local rb = has("ruby")
-
-  return rb
-end
-
-function eldin2()
-  local e1 = eldin1()
-  local m = has("mitts")
-  local b = has("bombs")
-  local h = has("hb")
-
-  return (e1 and (b or h or m))
-end
-
-function volcano()
-  local e2 = eldin2()
-  local fire = has("fse")
-
-  return (e2 and fire)
-end
-
-function lanayru1()
-  local am = has("amber")
-
-  return am
-end
-
-function lanayru2()
-  local l1 = lanayru1
-  local c = has("cs")
-  local h = has("hb")
-  return (l1 and (c or h))
-end
-
-function wheel()
-  local l1 = lanayru1()
-  local b = has("bombs")
-  return (l1 and b)
-end
-
-function sea()
-  local l = lanayru1()
-  local c = has("cs")
-  local sk = has("cavessk")
-
-  return (l and c and sk)
-end
-
-function walls()
-  local ac = skyview2_acc()
-  local bog = has("bog")
-  local harp = has("harp")
-
-  return (ac and bog and harp)
-end
-
-function yerbal()
-  local s = has("sling")
-  local bt = has("beetle")
-  local gb = has("bellows")
-  local w = has("wds")
-  local c = has("cs")
-  return ((s or bt) and ((gb and w) or c))
-end
-
-function floria()
-  local st = sealedtemple()
-  local w = has("wds")
-  local y = yerbal()
-  return (st and w and y)
+  return has("emerald")
 end
 
 function deepwoods()
-  local st = sealedtemple()
-  local sl = has("sling")
-  local c = has("cs")
-  local bt = has("beetle")
-  local bw = has("bow")
-  local bm = has("bombs")
-
-  return (st and (sl or c or bt or bw or bm))
+  return (sealedtemple() and ranged())
 end
 
-function dungeon_dw()
-  local dw = deepwoods()
-  return dw
+function floria()
+  return (sealedtemple() and wds() and yerbal())
 end
 
-function skyview0()
-  local s = has("sling")
-  local c = has("cs")
-  local bt = has("beetle")
-  local bw = has("bow")
-  local bm = has("bombs")
-  return (s or c or bt or bw or bm)
+function eldin1()
+  return has("ruby")
 end
 
-function skyview1()
-  local s = skyview0()
-  local w = has("wds")
-  return (s or w)
+function eldin2()
+  return (eldin1() and (bomgs() or hook() or dig()))
 end
+
+function volcano()
+  return (eldin2() and has("fse"))
+end
+
+function lanayru1()
+  return has("amber")
+end
+
+function lanayru2()
+  return (lanayru1() and (cs() or hook()))
+end
+
+function sea()
+  return (lanayru1() and cs() and has("cavessk"))
+end
+
+-- dungeon macros
+
+function skyview0() -- entrance of skyview
+  return ranged()
+end
+
+function skyview1() -- first room of skyview
+  return (skyview0() or wds())
+end
+
+function waterpod() -- water pods in fire sanc
+  return ranged()
+end
+
+-- dungeon beatable macros
 
 function skyview()
-  local acc = skyview_acc()
-  local bm = has("bombs")
-  local bt = has("beetle")
-  local bw = has("bow")
-  local w = has("wds")
-  local sk = has("skyviewsk",2)
-  local bk = has("skyviewbk")
-
-  return (acc and (bm or bt or bw or w) and sk and bk)
-end
-
-function dungeon_ev()
-  local e2 = eldin2()
-  local kp = has("kp",5)
-
-  return (e2 and kp)
-end
-
-function earth()
-  local acc = earth_acc()
-  local mt = has("mitts")
-  local bm = has("bombs")
-  local h = has("hb")
-  local bt = has("beetle")
-  local bk = has("earthbk")
-
-  return (acc and bm and (h or (bt and mt)) and bk)
-end
-
-function dungeon_ld()
-  local l1 = lanayru1()
-  local bm = has("bombs")
-  return (l1 and bm)
-end
-
-function mine()
-  local acc = mine_acc()
-  local h = has("hb")
-  local g = has("bellows")
-  local bk = has("minebk")
-  
-  return (acc and h and g and bk)
-end
-
-function gate()
-  local bog = has("bog")
-  local harp = has("harp")
-  local st = sealedtemple()
-
-  return (bog and harp and st)
-end
-
-function skyview2_acc()
-  local w = has("wds")
-  local acc = skyview_acc()
-  return (w and acc)
+  return (skyview_acc() and (bmb() or btl() or bow() or wds()) and has("skyviewsk",2) and has("skyviewbk"))
 end
 
 function skyview2()
-  local acc = skyview2_acc()
-  local sv = skyview()
-  return (acc and sv)
+  return (skyview2_acc() and skyview())
 end
 
-function sacredwater()
-  local bot = bottle()
-  local sv = skyview2()
-  
-  return (sv and bot)
+function earth()
+  return (earth_acc() and bmb() and (hook() or (btl() and mitts())) and has("earthbk"))
 end
 
-function dungeon_lf()
-  local f = floria()
-  return f
+function mine()
+  return (mine_acc() and hook() and gust() and has("minebk"))
 end
 
 function cistern()
-  local bk = has("cisternbk")
-  local sk = has("cisternsk",2)
-  local acc = cistern_acc()
-  local w = has("whip")
-  local c = has("cs")
-
-  return (acc and w and bk and (sk or c))
-end
-
-function dungeon_ss()
-  local chart = has("seachart")
-  local s = sea()
-
-  return (s and chart)
+  return (cistern_acc() and whip() and has("cisternbk") and (has("cisternsk",2) or cs()))
 end
 
 function sandship()
-  local acc = sandship_acc()
-  local bk = has("sandshipbk")
-  local b = has("bow")
-
-  return (acc and b and bk)
-end
-
-function dungeon_vs()
-  local v = volcano()
-  local bt = bottle()
-  local c = has("cs")
-
-  return (v and bt and c)
-end
-
-function waterpod()
-  local bw = has("bow")
-  local bt = has("beetle")
-  local c = has("cs")
-  local s = has("sling")
-  local bm = has("bombs")
-  return (bw or bt or c or s or bm)
+  return (sandship_acc() and bow() and has("sandshipbk"))
 end
 
 function sanc()
-  local sk = has("sancsk",3)
-  local mitts = has("mogma")
-  local bk = has("sancbk")
-  local acc = sanc_acc()
-  local h = has("hb")
-
-  return (acc and h and sk and mitts and bk)
-end
-
-function past()
-  local ms = has("ms")
-  local s = has("sanc")
-  local c = has("cistern")
-  
-  return (ms and s and c)
-end
-
-function levias()
-  local p = past()
-  return p
-end
-
-function floodedfaron()
-  local l = levias()
-  local w = has("wds")
-  return (l and w)
-end
-
-function bokobase()
-  local l = levias()
-  return l
-end
-
-function gorge()
-  local l1 = lanayru1()
-  local l = levias()
-  local c = has("cs")
-  return (l1 and l and c)
-end
-
-function thunder()
-  local g = gorge()
-  local bm = has("bombs") 
-  local c = has("cs") 
-  local h = has("hb") 
-  local w = has("whip") 
-  local p = past()
-  return (g and bm and c and h and w and p)
-end
-
-function dungeon_sl()
-  local st = has("trials")
-  local c = has("cs")
-  
-  return (st and c)
+  return (sanc_acc() and hook() and has("sancsk",3) and has("mogma") and has("sancbk"))
 end
 
 function skykeep()
-  local ac = skykeep_acc()
-  local sk = has("skykeepsk")
-  local c = has("cs")
-  local b = has("bow") 
-  local g = has("bellows")
-  local w = has("whip")
+  return (skykeep_acc() and has("skykeepsk") and cs() and bow() and gust() and whip())
+end
 
-  return (ac and sk and c and b and g and w)
+-- story/npc macros
+
+function beedle()
+  return (slng() or btl() or bow() or cs() or bmb())
+end
+
+function yerbal()
+  return ((slng() or btl()) and ((gust() and wds()) or cs()))
+end
+
+function skyview2_acc()
+  return (skyview_acc() and wds())
+end
+
+function sacredwater()
+  return (skyview2() and bottle())
+end
+
+function gate()
+  return (has("bog") and harp() and sealedtemple())
+end
+
+function past()
+  return (has("ms") and has("sanc") and has("cistern"))
+end
+
+function levias()
+  return past()
+end
+
+function wheel()
+  return (lanayru1() and bmb())
+end
+
+function floodedfaron()
+  return (levias() and wds())
+end
+
+function bokobase()
+  return levias()
+end
+
+function gorge()
+  return (lanayru1() and levias() and cs())
+end
+
+function thunder()
+  return (gorge() and bmb() and cs() and hook() and whip() and past())
 end
 
 function demise()
   local p = past()
   local s = skykeep()
-  return (p and s)
+  return (past() and skykeep())
 end
 
-function cube_initial()
-  local dw = deepwoods()
-  return dw
+--goddess cubes macros
+
+function cube_initial() -- initial cube
+  return deepwoods()
 end
 
 function cube_wgt() --west great tree
-  local st = sealedtemple()
-  local c = has("cs")
-  local g = has("bellows")
-  local w = has("wds")
-  return (st and (c or (g and w)))
+  return (sealedtemple() and (cs() or (gust() and wds())))
 end
 
 function cube_egtr() --east great tree rope
-  local st = sealedtemple()
-  local c = has("cs")
-  local g = has("bellows")
-  local w = has("wds")
-  return (st and (c or (g and w)))
+  return (sealedtemple() and (cs() or (gust() and wds())))
 end
 
 function cube_egtcs() --east great tree claw
-  local st = sealedtemple()
-  local c = has("cs")
-  local g = has("bellows")
-  local w = has("wds")
-  return (st and (c or (g and w)))
+  return (sealedtemple() and (cs() or (gust() and wds())))
 end
 
-function cube_dw()
-  local dw = deepwoods()
-  return dw
+function cube_dw() -- deepwoods
+  return deepwoods()
 end
 
 function cube_sv() --skyview
-  local dw = deepwoods() 
-  local c = has("cs")
-  return (dw and c)
+  return (deepwoods() and cs())
 end
 
 function cube_svs() --skyview spring
-  local sv = skyview()
-  return sv
+  return skyview()
 end
 
 function cube_fl() --floria
-  local f = floria()
-  return f
+  return floria()
 end
 
 function cube_fw() --floria waterfall
-  local f = floria() 
-  local c = has("cs")
-  return (f and c)
+  return (floria() and cs())
 end
 
 function cube_ee() --eldin entrance
-  local e1 = eldin1()
-  return e1
+  return eldin1()
 end
 
 function cube_mte() --mogma turf entrance
-  local e1 = eldin1()
-  return e1
+  return eldin1()
 end
 
 function cube_mt() --mogma turf
-  local e1 = eldin1()
-  return e1
+  return eldin1()
 end
 
 function cube_etw() --west of et
-  local mitts = has("digging")
-  local e2 = eldin2()
-
-  return (e2 and mitts)
+  return (eldin2() and dig())
 end
 
 function cube_ete() --east of et
-  local e2 = eldin2()
-  return e2
+  return eldin2()
 end
 
 function cube_es() --eldin slide
-  local e2 = eldin2()
-  return e2
+  return eldin2()
 end
 
 function cube_vs() --volcano summit
-  local v = volcano()
-  return v
+  return volcano()
 end
 
 function cube_sw() --summit waterfall
-  local v = volcano()
-  return v
+  return volcano()
 end
 
 function cube_fse() --sanc entrance
-  local bot = bottle()
-  local v = volcano() 
-  local c = has("cs")
-
-  return (v and bot and c)
+  return (volcano() and bottle() and cs())
 end
 
 function cube_lme() --mine entrance
-  local l1 = lanayru1()
-  return l1
+  return lanayru1()
 end
 
 function cube_so() --sand oasis
-  local l2 = lanayru2()
-  return l2
+  return lanayru2()
 end
 
 function cube_tot() --ride in tot
-  local l2 = lanayru2()
-  local h = has("hb")
-  return (l2 and h)
+  return (lanayru2() and hook())
 end
 
 function cube_sp() --desert secret passageway
-  local l2 = lanayru2()
-  local b = has("bombs") 
-  local c = has("cs")
-  return (l2 and b and c)
+  return (lanayru2() and bmb() and cs())
 end
 
 function cube_hbf() --hook beetle flight
-  local l1 = lanayru1()
-  local c = has("cs")
-  local b = has("bombs")
-  local h = has("hb")
-  return (l1 and (c or b or h))
+  return (lanayru1() and (cs() or bmb() or hook()))
 end
 
 function cube_ah() --ancient harbour
-  local s = sea()
-  return s
+  return sea()
 end
 
 function cube_sr() --skipper's retreat
-  local s = sea() 
-  local b = has("bombs") 
-  local h = has("hb")
-  local w = has("whip")
-  return (s and (b or h or w))
+  return (sea() and (bmb() or hook() or whip()))
 end
 
 function cube_ps() --pirate stronghold
-  local s = sea() 
-  local g = has("bellows")
-  return (s and g)
+  return (sea() and gust())
 end
 
 function cube_lg() --gorge
-  local g = gorge()
-  local c = has("cs")
-  local w = has("whip")
-  local h = has("hb")
-  return (g and c and w and h)
+  return (gorge() and cs() and whip() and hook())
 end
 
---framework for dungeon_ rando
+
+--framework for dungeon rando
 function skyview_acc()
   local acc = dungeon_dw()
   return acc
@@ -546,4 +365,33 @@ end
 function skykeep_acc()
   local acc = dungeon_sl()
   return acc
+end
+
+function dungeon_dw() -- dungeon entrance in deep woods
+  local dw = deepwoods()
+  return dw
+end
+
+function dungeon_ev() -- dungeon entrance in eldin volcano
+  return (eldin2() and has("kp",5))
+end
+
+function dungeon_ld() -- dungeon entrance in lanayru desert
+  return (lanayru1() and bmb())
+end
+
+function dungeon_lf() -- dungeon entrance in lake floria
+  return floria()
+end
+
+function dungeon_ss() -- dungeon entrance in sand sea
+  return (sea() and has("chart"))
+end
+
+function dungeon_vs() -- dungeon entrance in volcano summit
+  return (volcano() and bottle() and cs())
+end
+
+function dungeon_sl() -- dungeon entrance in skyloft
+  return (has("trials") and cs())
 end
